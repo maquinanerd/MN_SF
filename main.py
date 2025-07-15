3d10d1955e0fee860bef22a4552c4e2a12b606c0
maquinanerd <30965113+maquinanerd@users.noreply.github.com>
2025-07-15 17:06:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from app import app, db
-from models import Article, ProcessingLog
-
-# ⚠️ LIMPEZA DO BANCO DE DADOS (APAGAR ARTIGOS E LOGS)
-with app.app_context():
-    db.session.query(ProcessingLog).delete()
-    db.session.query(Article).delete()
-    db.session.commit()
-    print(">>> Banco de dados resetado com sucesso.")
+from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
